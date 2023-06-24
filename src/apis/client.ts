a8e3ddefe255a6276c45e47008d266da1adbb2f5
gym <3521834081@qq.com>
2023-06-24 19:39:02 +0800

    fix: 端口号

--- src/apis/client.ts
+++ src/apis/client.ts
@@ -1,9 +1,9 @@
 import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 const httpLink = createHttpLink({
-  uri: `http://${window.location.hostname}:3307/graphql`,
+  uri: `http://${window.location.hostname}:7000/graphql`,
 });
 export const apolloClient = new ApolloClient({
-  uri: `http://${window.location.hostname}:3307/graphql`,
+  uri: `http://${window.location.hostname}:7000/graphql`,
   link: httpLink,
   cache: new InMemoryCache({
     addTypename: false,
