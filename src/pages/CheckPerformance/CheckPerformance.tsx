455baf29fd51504921a8379238111e5834f3d653
SionTianYIn <tianyin1117@icloud.com>
2023-06-28 10:18:20 +0800

    modified:   package.json 	modified:   src/pages/CheckPerformance/CheckPerformance.tsx

--- src/pages/CheckPerformance/CheckPerformance.tsx
+++ src/pages/CheckPerformance/CheckPerformance.tsx
@@ -11,18 +11,16 @@ import type { MenuProps } from 'antd';
 import { Button, DatePicker, Input, Layout, Menu, Select, Table } from 'antd';
 import locale from 'antd/es/date-picker/locale/zh_CN';
 import ExcelJS from 'exceljs';
+import { DateTime } from 'luxon';
 import React, { useState } from 'react';
+
+import { getUserType } from '../../store/SaveToken';
 
 const { Column } = Table;
 
 const { RangePicker } = DatePicker;
 
-// // 解码JWT令牌，获取载荷中的角色信息
-// const decodedToken = jwt_decode(token);
-// const userRoles = decodedToken.roles;
-
-// // 检查用户角色是否包含superAdmin
-// const isSuperAdmin = userRoles.includes('superAdmin');
+const loginAuth = getUserType();
 
 const items: MenuProps['items'] = [
   {
@@ -36,123 +34,6 @@ const items: MenuProps['items'] = [
     className: 'UpperMenuItem',
   },
 ];
-
-// 一系列城市-镇-网格的数据，后面肯定需要修改，才能实现后端说的递归查询
-// 应该是每个label的value都是一个Int，这样方便GQL查询，到时候需要修改，并且修改状态
-const cityOptionData = [
-  {
-    label: '漠河市',
-    value: 'MoHeCity',
-  },
-];
-
-const townOptionData = [
-  {
-    label: '西林吉镇',
-    value: 'XiLinJi',
-  },
-  {
-    label: '古莲镇',
-    value: 'GuLian',
-  },
-  {
-    label: '图强镇',
-    value: 'TuQiang',
-  },
-  {
-    label: '阿木尔镇',
-    value: 'AMuEr',
-  },
-  {
-    label: '北极镇',
-    value: 'BeiJi',
-  },
-  {
-    label: '兴安镇',
-    value: 'XingAn',
-  },
-];
-
-// area_id
-const xiLinJiOptionData = [
-  {
-    label: '名苑社区',
-    value: 1,
-  },
-  {
-    label: '其他等等社区',
-    value: 2,
-  },
-];
-
-const guLianOptionsData = [
-  {
-    label: '古莲社区',
-    value: 3,
-  },
-  {
-    label: '其他等等社区',
-    value: 4,
-  },
-];
-
-const tuQiangOptionsData = [
-  {
-    label: '图强社区',
-    value: 5,
-  },
-  {
-    label: '其他等等社区',
-    value: 6,
-  },
-];
-
-const aMuErOptionsData = [
-  {
-    label: '阿尔姆社区',
-    value: 7,
-  },
-  {
-    label: '其他等等社区',
-    value: 8,
-  },
-];
-
-const beiJiOptionsData = [
-  {
-    label: '北极村',
-    value: 9,
-  },
-  {
-    label: '洛古河村',
-    value: 10,
-  },
-  {
-    label: '北红村',
-    value: 11,
-  },
-];
-
-const xingAnOptionsData = [
-  {
-    label: '兴安社区',
-    value: 12,
-  },
-  {
-    label: '其他等等社区',
-    value: 13,
-  },
-];
-
-// 当前权限
-// const Auth = {
-//   superAdmin: 1,
-//   filmPolice: 2,
-//   communityDirector: 3,
-//   gridMember: 4,
-//   Director: 5,
-//   gridLength: 6,
-// };
 
 // Query查询
 const GET_USER_LOG_OPERATIONS = gql`
@@ -193,9 +74,15 @@ const GET_USER_LOG_OPERATIONS = gql`
 `;
 
 const GET_DEFAULT_TABLEDATA = gql`
-  query GetUserLogOperations($role_id: Int!, $skip: Int, $take: Int) {
+query GetUserLogOperations(
+    $role_id: Int!
+    $skip: Int
+    $take: Int
+  ) {
     queryUserLogOperationList(
-      getOperationInput: { role_id: $role_id }
+      getOperationInput: {
+        role_id: $role_id
+      }
       skip: $skip
       take: $take
     ) {
@@ -215,8 +102,20 @@ const GET_DEFAULT_TABLEDATA = gql`
   }
 `;
 
+const GET_AREAS_QUERY = gql`
+  query GetAreas {
+    getArea {
+      id
+      level
+      name
+      parent_id
+    }
+  }
+`;
+
+
 const CheckPerformance: React.FC = () => {
-  const [current, setCurrent] = useState('mail');
+  const [current, setCurrent] = useState('');
 
   const onMenuClick: MenuProps['onClick'] = (e) => {
     setCurrent(e.key);
@@ -226,44 +125,41 @@ const CheckPerformance: React.FC = () => {
       case 'gridMember':
         setCurrentAuth(4);
         setIsDefault(true);
-        setIsSuperAdmin(true);
         break;
       case 'filmPolice':
         setCurrentAuth(2);
         setIsDefault(true);
-        setIsSuperAdmin(true);
         break;
-      default:
-        setCurrentAuth(0);
     }
   };
 
   // 一系列状态Hook，用来存储表单
   const [menuState, setMenuState] = useState(0);
   const [name, setName] = useState('');
-  const [city, setCity] = useState('default');
-  const [town, setTown] = useState('default');
-  const [grid, setGrid] = useState('default');
-  // const [gridID, setGridID] = useState(0);
+  const [city, setCity] = useState(0);
+  const [town, setTown] = useState(0);
+  const [grid, setGrid] = useState(0);
   const [beginTime, setBeginTime] = useState();
   const [endTime, setEndTime] = useState();
   // 同样是一系列状态Hook，Auth负责管理权限，isDefault负责展示初始表单，isSuperAdmin负责判断是否可以导出数据
-  const [currentAuth, setCurrentAuth] = useState(0);
+  const [currentAuth, setCurrentAuth] = useState(4);
   const [isDefault, setIsDefault] = useState(true);
-  const [isSuperAdmin, setIsSuperAdmin] = useState(false);
+  const areaID = grid !== 0
+  ? grid
+  : (town !== 0 ? town : city);
 
   // Datetime
   const handleRangeChange = (values: any) => {
-    if (values) {
-      const [start, end] = values;
-      const beginTimeString = start ? start.format('YYYY-MM-DDTHH:mm:ssZ') : undefined;
-      const endTimeString = end ? end.format('YYYY-MM-DDTHH:mm:ssZ') : undefined;
-      setBeginTime(beginTimeString);
-      setEndTime(endTimeString);
-    } else {
-      // Handle the case when values is null
-      setBeginTime(undefined);
-      setEndTime(undefined);
+  if (values) {
+    const [start, end] = values;
+    const beginTimeString = start ? start.format('YYYY-MM-DDTHH:mm:ssZ') : undefined;
+    const endTimeString = end ? end.format('YYYY-MM-DDTHH:mm:ssZ') : undefined;
+    setBeginTime(beginTimeString);
+    setEndTime(endTimeString);
+  } else {
+    // Handle the case when values is null
+    setBeginTime(undefined);
+    setEndTime(undefined);
     }
   };
 
@@ -290,13 +186,13 @@ const CheckPerformance: React.FC = () => {
       const tempMenu = menuState;
       if (tempMenu === 0) {
         // console.log('no menu selected');
-        alert('请选择网格员或民警');
+        // alert('请选择网格员或民警');
       } else {
         setIsDefault(false);
         fetchData({
           variables: {
             role_id: currentAuth,
-            area_id: 0 | parseInt(grid, 10),
+            area_id: areaID,
             name: name,
             begin_time: beginTime,
             end_time: endTime,
@@ -310,33 +206,21 @@ const CheckPerformance: React.FC = () => {
 
   // 这里的Select级联使用了switch的思想，使用useState得到的city town 进行选择，调取相应的数组
   // 如果想要加入新的区划，直接const创建新数组，然后再switch加入新条件即可
-  const getTownOptions = (city: string) => {
-    switch (city) {
-      case 'MoHeCity':
-        return townOptionData;
-      default:
-        return [];
-    }
+  const getTownOptions = (city: number) => {
+  // Filter level2Areas based on parent_id matching with city
+  const townOptions = level2Areas.filter(area => area.parent_id === city);
+  // Map townOptions to options format with label and value properties
+  return townOptions.map(town => ({ label: town.name, value: town.id }));
   };
+  
 
-  const getGridOptions = (town: string) => {
-    switch (town) {
-      case 'XiLinJi':
-        return xiLinJiOptionData;
-      case 'GuLian':
-        return guLianOptionsData;
-      case 'TuQiang':
-        return tuQiangOptionsData;
-      case 'AMuEr':
-        return aMuErOptionsData;
-      case 'BeiJi':
-        return beiJiOptionsData;
-      case 'XingAn':
-        return xingAnOptionsData;
-      default:
-        return [];
-    }
+  const getGridOptions = (town: number) => {
+  // Filter level3Areas based on parent_id matching with town
+    const gridOptions = level3Areas.filter(area => area.parent_id === town); 
+    // Map gridOptions to options format with label and value properties
+    return gridOptions.map(grid => ({ label: grid.name, value: grid.id })); 
   };
+  
 
   // 调用GQL语句，向后端查询数据
   const [fetchData, { error, data: SearchedData }] = useLazyQuery(
@@ -344,15 +228,33 @@ const CheckPerformance: React.FC = () => {
   );
 
   const { data: SearchedOrigin } = useQuery(GET_DEFAULT_TABLEDATA, {
-    variables: {
-      role_id: currentAuth,
-      skip: 1,
-      take: 10,
-    },
+  variables: {
+    role_id: currentAuth,
+    skip: 1,
+    take: 10,
+    }
   });
 
+  const { data: AreaData } = useQuery(GET_AREAS_QUERY);
+  const level1Areas = [];
+  const level2Areas = [];
+  const level3Areas = [];
+
+  if (AreaData && AreaData.getArea) {
+  AreaData.getArea.forEach((area: { level: number; }) => {
+    if (area.level === 1) {
+      level1Areas.push(area);
+    } else if (area.level === 2) {
+      level2Areas.push(area);
+    } else if (area.level === 3) {
+      level3Areas.push(area);
+    }
+  });     
+  }
+  
+
   // 抽取接口得到的data，这是我们需要填入表单中的数据
-  const DefaultData =
+  const DefaultData = 
     SearchedOrigin &&
     SearchedOrigin.queryUserLogOperationList &&
     SearchedOrigin.queryUserLogOperationList.data;
@@ -361,26 +263,17 @@ const CheckPerformance: React.FC = () => {
     SearchedData &&
     SearchedData.queryUserLogOperationList &&
     SearchedData.queryUserLogOperationList.data;
-
+  
   const UsedData = isDefault ? DefaultData : TableData;
 
   // 前端实现导出excel文档的设计，后端的嘛。。。不知道怎么调用
   const handleExport = async () => {
-    if (isSuperAdmin) {
+    if (loginAuth === 'superAdmin') {
       const workbook = new ExcelJS.Workbook();
       const worksheet = workbook.addWorksheet('Sheet 1');
 
-      const headerRow = worksheet.addRow([
-        '编号',
-        '姓名',
-        '登陆次数',
-        '查找次数',
-        '新增群众数',
-        '提交事件数',
-        '群众信息变更数',
-        '开始时间',
-        '结束时间',
-      ]);
+      const headerRow =
+      worksheet.addRow(['编号', '姓名', '登陆次数', '查找次数', '新增群众数', '提交事件数', '群众信息变更数', '开始时间', '结束时间']);
       headerRow.font = { bold: true };
       headerRow.alignment = { horizontal: 'center' };
 
@@ -388,25 +281,15 @@ const CheckPerformance: React.FC = () => {
       const data = UsedData;
 
       data.forEach((item: any, index: any) => {
-        worksheet.getCell(`A${index + 2}`).value = item.id;
+        worksheet.getCell(`A${index + 2}`).value = index + 1;
         worksheet.getCell(`B${index + 2}`).value = item.name;
-        worksheet.getCell(`C${index + 2}`).value = item.login_times;
+        worksheet.getCell(`C${index + 2}`).value = item.login_count;
         worksheet.getCell(`D${index + 2}`).value = item.query_count;
         worksheet.getCell(`E${index + 2}`).value = item.add_person_count;
         worksheet.getCell(`F${index + 2}`).value = item.submit_event_count;
         worksheet.getCell(`G${index + 2}`).value = item.modify_person_count;
-        worksheet.getCell(`H${index + 2}`).value = item.beginTime;
-        worksheet.getCell(`I${index + 2}`).value = item.endTime;
-        // 设置其他单元格...
-        // id
-        // name
-        // login_count
-        // modify_person_count = infoChanged
-        // query_count = searchTimes
-        // submit_event_count
-        // add_person_count = newPeople
-        // begin_time
-        // end_time
+        worksheet.getCell(`H${index + 2}`).value = DateTime.fromMillis(item.begin_time).toFormat('yyyy-MM-dd HH:mm:ss');
+        worksheet.getCell(`I${index + 2}`).value = DateTime.fromMillis(item.end_time).toFormat('yyyy-MM-dd HH:mm:ss');
       });
 
       worksheet.getColumn(1).width = 10;
@@ -422,9 +305,8 @@ const CheckPerformance: React.FC = () => {
 
       const buffer = await workbook.xlsx.writeBuffer();
 
-      const blob = new Blob([buffer], {
-        type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-      });
+      const blob = new Blob([buffer],
+        { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
 
       const url = URL.createObjectURL(blob);
       const link = document.createElement('a');
@@ -433,49 +315,31 @@ const CheckPerformance: React.FC = () => {
       link.click();
 
       URL.revokeObjectURL(url);
-    } else {
+    }
+    else { 
       alert('您没有权限导出！');
     }
   };
 
+ // 时间戳转换
+  const formatTimestamp = (timestamp: any) => {
+    const date = new Date(timestamp);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  };
+
   return (
     <Layout className="CpLayout">
-      {/* Serial Experiments
-      <div>
-        权限管理：
-        <Button onClick={() => {
-            // setCurrentAuth(Auth.superAdmin);
-            setIsSuperAdmin(true);
-        }}>超级管理员</Button>
-        <Button onClick={() => setCurrentAuth(Auth.Director)}>所长</Button>
-        <Button onClick={() => setCurrentAuth(Auth.filmPolice)}>民警</Button>
-        <Button onClick={() => setCurrentAuth(Auth.communityDirector)}>社区主任</Button>
-        <Button onClick={() => setCurrentAuth(Auth.gridLength)}>网格长</Button>
-        <Button onClick={() => setCurrentAuth(Auth.gridMember)}>网格员</Button>
-        <Button
-          onClick={() => {
-            console.log(currentAuth);
-          }}
-        >
-          Console.log权限
-        </Button>
-        <Button onClick={() => { 
-          console.log(DefaultData);
-        }}>Default Test</Button>
-        <Button onClick={() => { 
-          console.log(TableData);
-          console.log(SearchedOrigin);
-          console.log(SearchedData);
-        }}>Data Test</Button>
-        <Button onClick={() => {
-          console.log(client);
-          console.log(accessToken);
-          }}>header</Button>
-      </div> */}
-      {/* 网格员 警员 菜单 */}
+        {/* 网格员 警员 菜单 */}
       <Menu
         onClick={onMenuClick}
-        selectedKeys={[current]}
+        selectedKeys={current ? [current] : [items[0].key]}
         mode="horizontal"
         items={items}
         className="UpperMenu"
@@ -492,20 +356,17 @@ const CheckPerformance: React.FC = () => {
             setName(event.target.value);
           }}
         />
-        {/* <Input placeholder="城市占位" className="BlockType" /> */}
         <Select
           placeholder="请选择城市"
           className="BlockTypeGrid"
-          options={cityOptionData}
+          options={level1Areas.map(area => ({ label: area.name, value: area.id }))}
           onSelect={(value) => {
             // Find the selected option based on the value
-            const selectedOption = cityOptionData.find(
-              (option) => option.value === value,
-            );
+            const selectedOption = level1Areas.find(area => area.id === value);
             if (selectedOption) {
-              setCity(selectedOption.value);
-              setTown('default');
-              setGrid('default');
+              setCity(selectedOption.id);
+              setTown(0);
+              setGrid(0);
             }
           }}
         />
@@ -515,49 +376,31 @@ const CheckPerformance: React.FC = () => {
           options={getTownOptions(city)}
           onSelect={(value) => {
             // Find the selected option based on the value
-            const selectedOption = getTownOptions(city).find(
-              (option) => option.value === value,
-            );
+            const selectedOption = getTownOptions(city).find(option => option.value === value);
             if (selectedOption) {
               setTown(selectedOption.value);
-              setGrid('default');
+              setGrid(0);
             }
           }}
-          value={town !== 'default' ? town : null || null}
-        />
-        {/* 这里有warning，但是不要随便修改 */}
+          value={town !== 0 ? town : null || null}
+        /> 
         <Select
-          placeholder="请选择网格"
+          placeholder="请选择社区"  
           className="BlockTypeGrid"
-          options={getGridOptions(town)}
+          options={getGridOptions(town)}   
           onSelect={(value) => {
-            // Find the selected option based on the value
+            // Find the selected option based on the value    
             const selectedOption = getGridOptions(town).find(
-              (option) => option.value?.toString() === value,
-            );
-            if (selectedOption) {
-              // @ts-ignore
-              setGrid(selectedOption.value);
-            }
-          }}
-          value={grid !== 'default' ? grid : null || null}
+              option => option.value === value);       
+            if (selectedOption) {      
+              setGrid(selectedOption.value);       
+            }       
+          }}       
+          value={grid !== 0 ? grid : null || null}    
           // Set the value to the first option's value only if grid is not 'default'
           // 当重新选择上一个Select选项后，该选项需要重新选择
         />
-        {/*
-        // 备选的时间选择模块，但由于需要判断开始时间时候比结束时间晚，所以用了AntD中省事的RangePicker
-         <DatePicker
-          format="YYYY-MM-DD HH:mm:ss"
-          showTime={{ defaultValue: dayjs('00:00:00', 'HH:mm:ss') }}
-          placeholder="开始时间"
-          className="DateBlockType"
-        />
-        <DatePicker
-          format="YYYY-MM-DD HH:mm:ss"
-          showTime={{ defaultValue: dayjs('00:00:00', 'HH:mm:ss') }}
-          placeholder="结束时间"
-          className="DateBlockType"
-        /> */}
+        
         <RangePicker
           showTime
           locale={locale}
@@ -585,9 +428,10 @@ const CheckPerformance: React.FC = () => {
         <Table dataSource={UsedData} style={{ margin: '0px 0.5vw' }}>
           <Column
             title={<div style={{ textAlign: 'center' }}>编号</div>}
-            dataIndex="id"
-            key="id"
+            // dataIndex="id"
+            // key="id"
             align="center"
+            render={(text, record, index) => index + 1}
           />
           <Column
             title={<div style={{ textAlign: 'center' }}>姓名</div>}
@@ -627,15 +471,17 @@ const CheckPerformance: React.FC = () => {
           />
           <Column
             title={<div style={{ textAlign: 'center' }}>开始时间</div>}
-            dataIndex="begin_Time"
+            dataIndex="begin_time"
             key="begin_Time"
             align="center"
+            render={(text) => formatTimestamp(text)}
           />
           <Column
             title={<div style={{ textAlign: 'center' }}>结束时间</div>}
-            dataIndex="end_Time"
+            dataIndex="end_time"
             key="end_Time"
             align="center"
+            render={(text) => formatTimestamp(text)}
           />
         </Table>
       </div>
