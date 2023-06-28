7c2545fa41f416246f3fd2a2f15c8f17edd6424d
gym <3521834081@qq.com>
2023-06-28 10:46:56 +0800

    fix: 格式

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -43,7 +43,7 @@ const Login = () => {
             window.location.href = newUrl;
           }, 300);
         })
-        .catch((_err) => {
+        .catch(() => {
           message.open({
             type: 'error',
             content: '账号或密码错误',
