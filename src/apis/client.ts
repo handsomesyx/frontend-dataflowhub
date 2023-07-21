74009a2e2da6cf875320c057c6c71583aa7ca786
Endwina <446044331@qq.com>
2023-07-21 18:05:13 +0800

    fix: 修复

--- src/apis/client.ts
+++ src/apis/client.ts
@@ -98,8 +98,8 @@ const errorLink = onError(({ graphQLErrors, networkError, operation, forward }) 
 });
 
 export const apolloClient = new ApolloClient({
-  uri: `http://${window.location.hostname}:7000/graphql`,
-  // uri: '/graphql',
+  // uri: `http://${window.location.hostname}:7000/graphql`,
+  uri: '/graphql',
   link: ApolloLink.from([errorLink, authLink, httpLink]),
   cache: new InMemoryCache({
     addTypename: false,
