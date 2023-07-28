bd69c94961bedc0905893ec8345131010042e4cd
Endwina <446044331@qq.com>
2023-07-28 14:04:14 +0800

    fix: 事件管理分页修复

--- src/pages/IncidentManagement/index.tsx
+++ src/pages/IncidentManagement/index.tsx
@@ -40,7 +40,7 @@ function IncidentManagement() {
     },
   ];
   return (
-    <div style={{ width: '100%', height: '100%' }}>
+    <div style={{ width: '100%', height: '100%', overflowY: 'auto' }}>
       <Tabs defaultActiveKey="1" items={items} onChange={onChange} />
     </div>
   );
